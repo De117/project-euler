ecf2c4142184fc1a353dfe758ef06d2ed4cadfe0
Antun Maldini <antun.maldini@gmail.com>
2024-03-27 01:34:37 +0100

    Write up problem 63 (Python) properly, and use correct limits

--- python/problem_63.py
+++ python/problem_63.py
@@ -8,12 +8,28 @@
 # For all n, 10**n is an (n+1)-digit integer. 10 as a base is out, for all n.
 # So are bases > 10.
 # Valid bases are 1 to 9 inclusive.
-
+#
 # Now, consider how B**n relates to its own length.
-# We can observe that every B starts a bit too short,
-# then enters a zone where it's about the right length,
-# then becomes too long and stays that way.
+#
+# For all bases, B**1 will have one digit, which satisfies the property.
+# The number of digits -- or rather, the base-10 logarithm of the number, which
+# we round up to get the number of digits -- grows, but at a slower rate than 1
+# every iteration, and thus falls behind the growth of n, forever.
+#
+# So for every base, we start at n=1, and once the property stops holding, we quit.
 
-# A range of 100 turns out to be more than enough.
-integers = {i**j for i in range(1,10) for j in range(100) if len(str(i**j)) == j}
+from math import ceil, log10
+
+integers = {1}  # ceil(log10(1**n)) = 0, so it's not covered below
+
+for i in (2,3,4,5,6,7,8,9):
+    j = 0
+    while True:
+        j += 1
+        number_of_digits = ceil(log10(i**j))
+        if number_of_digits == j:
+            integers.add(i**j)
+        if number_of_digits < j:
+            break
+
 print(len(integers))
