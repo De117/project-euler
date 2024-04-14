911a134dd025a76caed3bb8152804354843d89fa
Antun Maldini <antun.maldini@gmail.com>
2024-04-14 23:46:20 +0200

    Solve problem 69 in Python

---
+++ python/problem_69.py
@@ -0,0 +1,37 @@
+# Problem 69 (Totient Maximum)
+# ============================
+#
+# Euler's totient function, Φ(n) [sometimes called the phi function],
+# is defined as the number of positive integers not exceeding n which
+# are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are
+# all less than or equal to nine and relatively prime to nine, Φ(9) = 6.
+#
+#  n  |  Relatively Prime  |  Φ(n) | n/Φ(n)
+# ----+--------------------+-------+--------
+#  2  |    1               |   1   |  2
+#  3  |    1,2             |   2   |  1.5
+#  4  |    1,3             |   2   |  2
+#  5  |    1,2,3,4         |   4   |  1.25
+#  6  |    1,5             |   2   |  3
+#  7  |    1,2,3,4,5,6     |   6   |  1.1666...
+#  8  |    1,3,5,7         |   4   |  2
+#  9  |    1,2,4,5,7,8     |   6   |  1.5
+#  10 |    1,3,7,9         |   4   |  2.5
+#
+# It can be seen that n=6 produces a maximum n/Φ(n) for n ≤ 10.
+#
+# Find the value of n ≤ 1_000_000 for which n/Φ(n) is a maximum.
+from common import PrimeDecomposition
+from fractions import Fraction
+from functools import reduce
+import operator
+
+product = lambda xs: reduce(operator.mul, xs, 1)
+
+def euler_phi(n: int) -> int:
+    """Number of positive integers coprime to n."""
+    # For background, see: https://en.wikipedia.org/wiki/Euler%27s_totient_function
+    s = n * product(Fraction(p - 1, p) for p in PrimeDecomposition(n).coefficients.keys())
+    return int(s)
+
+print(max(range(1, 1_000_000+1), key=lambda n: n/euler_phi(n)))
